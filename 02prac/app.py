94cdfe0971a179fad69bdec18ef456f8f9ed4a87
DAEJIN JEONG <jjdj0315@gmail.com>
2025-05-17 13:38:39 +0900

    finish : 02prac

--- 02prac/app.py
+++ 02prac/app.py
@@ -1,25 +1,18 @@
+# main
 from base64 import b64encode
 
 import streamlit as st
+
 from utils.utils import chain_recommend_food, chain_recommend_wine
 
-# Title of the web app
 st.title("Food and Wine Pairing")
 
-# Radio buttons for selecting pairing type
-pairing_type = st.radio(
-    "Choose Pairing Type:",
-    ("Food Pairing", "Wine Pairing")
-)
+pairing_type = st.radio("choose Pairing Type", ("Food Pairing", "Wine Pairing"))
 
-
-# Text input for description
 description = st.text_input("Description")
 
-
 upload_type = st.radio(
-    "Choose upload type: (Support formats: jpeg, jpg, gif, png)",
-    ("Image", "URL")
+    "Choose upload type: (Support formats: jpeg, jpg, gif, png)", ("Image", "URL")
 )
 if upload_type == "Image":
     # File uploader for attachment
@@ -27,18 +20,26 @@ if upload_type == "Image":
 else:
     # Text input for image URL
     image_url = st.text_input("Image URL")
-    
-    
+
 if st.button("Pair"):
     if upload_type == "Image" and uploaded_file is not None:
         base64_file = b64encode(uploaded_file.read()).decode("utf-8")
-        image_urls = [f"dataLimage/png;base64,{base64_file}"]
-    
-elif upload_type == "URL" and image_url:
-    image_urls = [image_url]
-else:
-    image_url = None
-    
+        image_urls = [f"data:image/png;base64,{base64_file}"]
+    elif upload_type == "URL" and image_url:
+        image_urls = [image_url]
+    else:
+        image_urls = None
 
-if pairing_type == "Food Pairing":
-    chain == chain_recommend_food()
+    # Example processing (this is where the pairing logic would go)
+    if pairing_type == "Food Pairing":
+        chain = chain_recommend_food()
+    else:
+        chain = chain_recommend_wine()
+
+    response = chain.invoke(
+        {
+            "query": description,
+            "image_urls": image_urls,
+        }
+    )
+    st.write(response)
